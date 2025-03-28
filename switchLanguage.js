9f4ec8fd21a5b1850f13336d41069dee48559a77
DGrothe-PhD <dag_fs@t-online.de>
2025-03-28 16:25:03 +0100

    set default language, show selected language in the menu

--- switchLanguage.js
+++ switchLanguage.js
@@ -1,13 +1,39 @@
-let language = navigator.language;
-      async function loadJson(file) {
-        let text = "";
-        const response = await fetch(file);
-        if (!response.ok){text = "";} else {text = response.json();}
-        return text;
+let defaultLanguage = navigator.language;
+let selectedLang = navigator.language;
+
+async function loadJson(file){
+  let text="";
+  const response = await fetch(file);
+  if(!response.ok){text="";}else{text=response.json();}
+  return text;
 }
+
+function setDefaultLanguage(){
+  try{
+    const links = document.querySelectorAll('link[rel="alternate"]');
+    let fallBackLink = null;
+    for (let link of links) {
+      if (link.getAttribute("hreflang") === defaultLanguage) {
+        window.location.href = link.getAttribute("href");
+        return;
+      }
+      // Fallback for any language this coder doesn't speak.
+      if (link.getAttribute("hreflang").slice(0,2).toLowerCase() === "en"){
+        fallBackLink = link;
+      }
+    }
+    if(fallBackLink != null){
+      window.location.href = fallBackLink.getAttribute("href");
+    }
+  }
+  catch(error){
+    alert("oops");
+  }
+}
+
 function switchLanguage(event) {
   event.preventDefault();
-  const selectedLang = document.getElementById("language").value;
+  selectedLang = document.getElementById("language").value;
   const links = document.querySelectorAll('link[rel="alternate"]');
 
   for (let link of links) {
@@ -16,6 +42,4 @@ function switchLanguage(event) {
       return;
     }
   }
-
-  //alert("Die gewünschte Sprache ist nicht verfügbar.");
 }
